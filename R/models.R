9f5d09139306bf1276c25e4b5fc5a00632744215
dtkaplan <dtkaplan@gmail.com>
2016-03-14 19:16:47 -0500

    working version of D.lm

--- R/models.R
+++ R/models.R
@@ -47,45 +47,56 @@ fmodel <- function(model=NULL, formula = NULL, data = NULL, nlevels = 3, ...) {
     facet_wrap(explan_vars[3])
 }
 
+#' Extract parts of models
+#'
+#' Functions for extracting data and names of explanatory and response variables from a model object
+#' @rdname extract_from_model
+#' @param object the model you are extracting features from.
+#' @param ... additional arguments (not used)
 #' @export
 data_from_model <- function(object, ...) {
   UseMethod("data_from_model")
 }
+#' @rdname extract_from_model
 #' @export
 explanatory_vars <- function(object, ...) {
   UseMethod("explanatory_vars")
 }
+#' @export
 explanatory_vars.lm <-
   explanatory_vars.groupwiseModel <-
   explanatory_vars.rpart <-
   explanatory_vars.glm <- function(object, ...) all.vars(object$terms[[3]])
-
-
+#' @rdname extract_from_model
 #' @export
 response_var <- function(object, ...) {
   UseMethod("response_var")
 }
+#' @export
 response_var.lm <-
   response_var.groupwiseModel <-
   response_var.rpart <-
   response_var.glm <- function(object, ...) { all.vars(object$terms[[2]])}
-#'
+#' @export
 data_from_model.groupwiseModel <-
   data_from_model.glm <- function(object, ...){
   object$data
 }
-#'
+#' @export
 data_from_model.lm <- function(object, ...) {
   object$model
 }
-#'
+#' @export
 data_from_model.rpart <- function(object, ...) {
   stop("Can't extract data from models of class rpart.")
 }
 
+#' @param data a data frame
+#' @param n number of values for specified variables: a list
+#' @param at optional values at which to set values
 
 #' @export
-reference_values <- function(data, n = 1, convert_fun = FALSE) {
+reference_values <- function(data, n = 1, at = list()) {
   var_names <- names(data)
   # n might be a list.  If so, the default should be 1
   n_default <- ifelse(inherits(n, "list"), 1, n)
@@ -94,7 +105,7 @@ reference_values <- function(data, n = 1, convert_fun = FALSE) {
   if (inherits(n, "list")) # override any appearing in the n-list
     n_values[names(n)] <- n
 
-  get_range <- function(var_name, n = 1) {
+  get_range <- function(var_name, n) {
     # get an appropriate set of levels
     values <- data[[var_name]]
     conversion <- NA
@@ -124,8 +135,14 @@ reference_values <- function(data, n = 1, convert_fun = FALSE) {
   names(ranges) <- var_names
   for (k in 1:length(var_names)) {
     # get the appropriate number of levels for each variable
-    ranges[[k]] <- get_range(var_names[k], n_values[[ var_names[k] ]])
-    conversions[[k]] <- attr(ranges[[k]], "convert")
+    if (var_names[k] %in% names(at)) {
+      ranges[[k]] <- at[[var_names[k]]]
+      conversions[[k]] <- ifelse(is.numeric(ranges[[k]]), "as.descrete", NA)
+
+    } else {
+      ranges[[k]] <- get_range(var_names[k], n_values[[ var_names[k] ]])
+      conversions[[k]] <- attr(ranges[[k]], "convert")
+    }
   }
   res <- do.call(expand.grid, ranges)
   attr(res, "convert") <- conversions
@@ -145,3 +162,127 @@ convert_to_discrete <- function(data) {
 
   data
 }
+
+#' Calculate effect sizes
+#'
+#' Like a derivative or finite-difference
+
+#' @param at center values for evaluating the model
+#' @param step list of numbers for numerical finite-difference, or comparison groups for
+#' @param raw for 2nd-order effects, show the sets of first-order effects
+#' @param ... additional arguments for \code{predict()}
+#' categorical variables.
+#'
+
+#' @export
+D.lm <- function(model, formula, at = NULL, step = NULL, raw = FALSE, ... ) {
+  # grab the explanatory variable to use for the difference
+  change_vars <- all.vars(mosaic::rhs(formula))
+
+  data <- fchart:::data_from_model.lm(model)
+  response <- fchart:::response_var.lm(model)
+  explan_vars <- fchart:::explanatory_vars.lm(model)
+
+  centers <- as.list(rep(NA, length(explan_vars)))
+  names(centers) <- explan_vars
+  for (var_name in explan_vars) {
+    values <- data[[var_name]]
+    centers[[var_name]] <-
+      if (is.numeric(values)) {
+        median(values, na.rm = TRUE)
+      } else {
+        # get the most popular level
+        names(table(sort(values, decreasing = TRUE)))[1]
+      }
+  }
+
+  # loop over <at> and update any nominal values
+  for (k in seq_along(at)) {
+    nm <- names(at)[k]
+    if ( ! nm %in% explan_vars) stop(nm, "isn't an explanatory variable.")
+    centers[[nm]] <- at[[k]]
+  }
+
+  change_ranges <- as.list(rep(NA, length(change_vars)))
+  names(change_ranges) <- change_vars
+  change_ranges[names(step)] <- step
+  for (vname in change_vars) {
+    if ( ! vname %in% names(step)) {
+      vals <- data[[vname]]
+      if (is.numeric(vals)) change_ranges[[vname]] <- sd(vals, na.rm = TRUE)
+      else {
+        center_category <- centers[[vname]]
+        new_categories <- names(sort(table(vals)))
+        new_categories <- setdiff(new_categories, center_category)
+        change_ranges[[vname]] <- new_categories[1:pmin(length(new_categories), 10)]
+      }
+    }
+
+    if (is.numeric(centers[[vname]]))
+      centers[[vname]] <- centers[[vname]] + c(-1, 1) * change_ranges[[vname]]
+    else
+      centers[[vname]] <- c(centers[[vname]], change_ranges[[vname]])
+  }
+
+  input_data <- reference_values(data, at = centers)
+
+  mod_vals <- predict(model, newdata = input_data)
+  input_data[[response]] <- mod_vals
+  # Set the response variable
+  # PROCESS
+  # for the variables in formula, each has a high and low value
+  # set the nominals for that variable to be high value
+  #  - calculate the model output
+  # set the nominals for the variable to be the low value(s) --- plural if categorical
+  #  - calculate the model output
+  # subtract the low value(s) from the high values
+
+  # If there is a second variable
+  # perform the process for the high and low values of the second variable
+  # just let there be 2 values for the second variable
+
+  one_change <- function(in_data, var_name, response_names) {
+    var_levels <- in_data[[var_name]]
+    ref_level <- var_levels[1]
+    remaining_levels <- var_levels[-1]
+    response <- in_data[response_names]
+    ref_val <- response[1,]
+    others <- response[-1,]
+    effect <- ref_val - others
+    if (is.numeric(ref_level)) {
+      res <- effect / (ref_level - remaining_levels)
+      names(res) <- paste0("delta_", response_names, "_by_", var_name)
+    } else {
+      res <- - effect
+      names(res) <- paste0(response_names, "..from..", ref_level, "..to..", remaining_levels)
+    }
+
+    res
+  }
+
+  if (length(change_vars) == 1) {
+    if (raw) effects <- input_data
+    else effects <- one_change(input_data, change_vars, response)
+  } else if (length(change_vars) == 2) {
+    effects <- NULL
+    vals <- unique(input_data[[change_vars[[2]]]])
+    if (is.factor(vals)) vals <- as.character(vals)
+    input_data <- split(input_data, input_data[[change_vars[2]]])
+    for (k in 1:length(input_data)) {
+      res <- one_change(input_data[[k]], change_vars[[1]], response)
+      effect <- data.frame(as.list(res))
+      row.names(effect) <- NULL
+      names(effect) <- names(res)
+      effects <- rbind(effects, data.frame(effect, vals[k], stringsAsFactors = FALSE))
+    }
+    effects <- data.frame(effects, stringsAsFactors = FALSE)
+    names(effects)[ncol(effects)] <- change_vars[[2]]
+    if ( ! raw) effects <- one_change(effects, change_vars[2], names(effects)[-length(names(effects))])
+  } else {
+    stop("Only 1st and 2nd-order interactions supported.")
+  }
+
+  effects
+}
+
+
