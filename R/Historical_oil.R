113ca9e733d9f9738fe181de861ed54bd48795d9
dtkaplan <dtkaplan@gmail.com>
2016-05-31 13:30:09 -0500

    Added Historical_oil

---
+++ R/Historical_oil.R
@@ -0,0 +1,24 @@
+#' Historical production of crude oil, worldwide 1880-2014
+#' 
+#' Annual production of crude oil, in millions of barrels (mbbl).
+#' 
+#' @docType data
+#' @name Historical_oil
+#' @usage data(Historical_oil, package = "statisticalModeling")
+#'
+#' @keywords datasets
+#'
+#' @format
+#'   A data frame with 47 cases, each of which is a US state, with observations on the following variables.
+#'   \itemize{
+#'     \item{\code{year}} {the year for which production is reported}
+#'     \item{\code{mbbl}} {oil production in millions of barrels}
+#'   }
+#'   
+#' @source Assembled from older information from  RH Romer (1976) "Energy: An Introduction to Physics" and more recent
+#' data from \code{data.oecd.org}.
+#' 
+#' @examples
+#' model <- lm(log(mbbl) ~ year, data = Historical_oid)
+#' fmodel(model)
+"Historical_oil"
