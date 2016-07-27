1ebde40ed1b076941277f231a2095e27efe878c1
Nick Carchedi <nick.carchedi@gmail.com>
2016-07-27 18:19:45 -0400

    fix bug in data_from_model()

--- R/models.R
+++ R/models.R
@@ -62,7 +62,7 @@ data_from_model.lm <-
     # if the object has a data attribute added by train, use that
     data_in_call <- which("data" == names(object$call))
     if (length(data_in_call) == 1) {
-      the_data <- eval(object$call[[data_in_call]])
+      the_data <- eval(object$call[[data_in_call]], envir = parent.frame(3))
       if (is.data.frame(the_data)) return(the_data)
     }
     # Fallback operation
